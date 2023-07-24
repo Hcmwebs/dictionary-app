e9832715bd12e8469c4dfd03f6a9a475d702a807
Heze <64218603+Hcmwebs@users.noreply.github.com>
2023-07-24 15:41:49 +1200

    tried out the api
    Signed-off-by: Heze <64218603+Hcmwebs@users.noreply.github.com>

--- src/context/DictionaryGlobalApp.jsx
+++ src/context/DictionaryGlobalApp.jsx
@@ -1,10 +1,12 @@
 import React, { createContext, useEffect, useContext, useState } from 'react'
-const url = 'https://api.dictionaryapi.dev/api/v2/entries/en/'
+const url = 'https://api.dictionaryapi.dev/api/v2/entries/en'
 
 const DictionaryAppContext = createContext()
 
 const DictionaryAppProvider = ({ children }) => {
-	const [word, setWord] = useState('hello')
+	const [word, setWord] = useState('binary')
+	const tempUrl = `url/${word}`
+	console.log(tempUrl)
 	return (
 		<DictionaryAppContext.Provider value={{ word }}>
 			{children}
