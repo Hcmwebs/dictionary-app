bd6f6714dceccaa658bd0168aaed4177a386fbab
Heze <64218603+Hcmwebs@users.noreply.github.com>
2023-12-13 16:54:19 +1300

    hooked tailwind and DaisyUi

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
-import React from 'react'
-import { useGlobalContext } from './context/DictionaryGlobalApp'
-import { Header, Mainbody } from './components'
+import React from 'react';
+import { useGlobalContext } from './context/DictionaryGlobalApp';
+import { Header, Mainbody } from './components';
 
 const App = () => {
-  const{word} = useGlobalContext()
-  return (
-    <div>
-      <Header />
-      <Mainbody />
-    </div>
-  )
-}
+	const { word } = useGlobalContext();
+	return (
+		<div>
+			<h1 className='text-3xl font-bold underline'>We are on with Tailwind </h1>
+			<button class='btn btn-primary'>Button</button>
+			<Header />
+			<Mainbody />
+		</div>
+	);
+};
 
-export default App
+export default App;
